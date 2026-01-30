33a0f101a800fd2fbb66ffc68bc4bbbe341a171a
Mon Albert Gamil <mrgamilmonalbert@gmail.com>
2026-01-31 01:54:12 +0800

    chore: add all exportables

--- packages/index.ts
+++ packages/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+export type { InferModelFromAttributes } from './utils/define-model';
+export type { ColumnSchemaDescriptor, DataTypeCategory } from './core/column-builder';
+export type { InferSelectFromAttributes, InferSelectFromModel } from './utils/create-select-schema';
+export type { InferInsertFromAttributes, InferInsertFromModel } from './utils/create-insert-schema';
+export type { InferUpdateFromAttributes, InferUpdateFromModel } from './utils/create-update-schema';
+
+export * from './core/column-factory';
+
+export { defineModel } from './utils/define-model';
+export { createSelectSchema } from './utils/create-select-schema';
+export { createInsertSchema } from './utils/create-insert-schema';
+export { createUpdateSchema } from './utils/create-update-schema';
